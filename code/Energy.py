e3f3301d61247e2578b7b1fdd6984a52fd96b218
Magnus Hagmar <hagmar.magnus@gmail.com>
2014-11-23 17:59:14 +0100

    Complete rewrite of main. Full model is created, but with duplicate representations of energy types. Value calculation is not fully implemented yet.

--- code/Energy.py
+++ code/Energy.py
@@ -1,33 +1,40 @@
 class Sector:
-    def __init__(self, name):
-        self.name = name
-        self.energies = {}
+	def __init__(self, name):
+		self.name = name
+		self.energies = {}
 
-    def add_energy(self, name, energy):
-        self.energies[name] = energy
+	def add_energy(self, name, energy):
+		self.energies[name] = energy
 
-    def value(self):
-        return sum([self.energies[e].value() for e in self.energies])
+	def value(self):
+		return sum([self.energies[e].value() for e in self.energies])
 
 class Energy:
-    def __init__(self, name, energy=0, quota=0, efficiency=0):
-        self.name = name
-        self.sectors = {}
-        self.inputs = {}
-        self.energy = energy
-        self.quota = quota
-        self.efficiency = efficiency
+	def __init__(self, name, energy=0, quota=100.0, efficiency=100.0):
+		self.name = name
+		self.sectors = {}
+		self.inputs = {}
+		self.energy = energy
+		self.quota = quota
+		self.efficiency = efficiency
 
-    def add_input(self, name, energy=0, quota=0, efficiency=0):
-        self.inputs[name] = Energy(name, energy, quota, efficiency)
+	def add_input(self, name, quota, efficiency):
+		new_energy = Energy(name, self.energy*quota/efficiency, quota, efficiency)
+		self.inputs[name] = new_energy
+		return new_energy
+	
+	def add_existing_input(self, energy):
+		energy.energy = self.energy*energy.quota/energy.efficiency
+		self.inputs[energy.name] = energy
 
-    def add_sector(self, name):
-        self.sectors[name] = Sector(name)
-        self.sectors[name].add_energy(self.name, self)
+	def add_sector(self, sector):
+		#self.sectors[name] = Sector(name)
+		self.sectors[name] = sector
+		self.sectors[name].add_energy(self.name, self)
 
-    def value(self, sector=None):
-        if sector:
-            return self.sectors[sector].value()
-        else:
-            return (sum([self.inputs[i].value() for i in self.inputs]) + self.energy) * self.quota / self.efficiency
+	def value(self, sector=None):
+		if sector:
+			return self.sectors[sector].value()
+		else:
+			return self.energy * self.quota / self.efficiency
 
--- code/main.py
+++ code/main.py
@@ -4,33 +4,49 @@ from copy import copy
 import json
 
 def main():
-    if len(argv) < 2:
-        print("Usage: 'python3 %s <json-file>' json-file containing data about the system." % argv[0])
-        exit(1)
+	'''if len(argv) < 2:
+		print("Usage: 'python3 %s <json-file>' json-file containing data about the system." % argv[0])
+		exit(1)
+	'''
+	with open("system-data.json", "r") as fp:
+		obj = json.load(fp)
 
-    with open(argv[1], "r") as fp:
-        obj = json.load(fp)
+	sectors = {}
+	energies = {}
+	primaryenergies = {}
+	energy = {}
 
-    sectors = {}
-    energies = {}
-    primaryenergies = {}
-    energy = {}
+	for energy in obj["primaryenergies"]:
+		primaryenergies[energy] = Energy(obj["primaryenergies"][energy]["name"])
 
-    for energy in obj["primaryenergies"]:
-        primaryenergies[energy] = Energy(obj["primaryenergies"][energy]["name"])
+	energies = copy(primaryenergies)
+	sectors = build_model(obj)
+	return sectors
+	#for energy in obj["sectors"][sector]["inputs"]:
+	#	energies[energy].add_input()
+	
+	#print(energies)
+	#print(sectors)
 
-    energies = copy(primaryenergies)
+def build_model(obj):
+	sectors = {}
+	for sector in obj["sectors"]:
+		sectors[sector] = Sector(obj["sectors"][sector]["name"])
+		
+		for input_name in obj["sectors"][sector]["inputs"]:
+			input = obj["sectors"][sector]["inputs"][input_name]
+			energy = Energy(input["name"], energy=input["energy"], efficiency=input["efficiency"])
+			sectors[sector].add_energy(input["name"], energy)
+			add_inputs(input, energy)
+	
+	return sectors
 
-    for sector in obj["sectors"]:
-        pass
-        sectors[sector] = Sector(obj["sectors"][sector]["name"])
-
-        for energy in obj["sectors"][sector]["inputs"]:
-            energies[energy].add_input()
-
-
-    print(energies)
-    #print(sectors)
+def add_inputs(obj, parent):
+	if "inputs" in obj:
+		for input_name in obj["inputs"]:
+			input = obj["inputs"][input_name]
+			new_energy = parent.add_input(input["name"], quota=input["quota"], efficiency=input["efficiency"])
+			add_inputs(input, new_energy)
 
 if __name__ == "__main__":
-    main()
+	main()
