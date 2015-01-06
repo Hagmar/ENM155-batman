49a8bf534b26ac1d2bbfc609b1d2b1a0c8744451
Magnus Hagmar <hagmar.magnus@gmail.com>
2015-01-06 13:35:22 +0100

    You can now check value (used and created energy) for a sector with the -v flag. Also, no longer crashes when trying to check value for a nonexistant energy or sector.

--- code/main.py
+++ code/main.py
@@ -41,18 +41,32 @@ def main():
 		for e in energies.values():
 			print u"{:16} id: {:15}".format(e.name, e.id)
 	elif args.values:
+		# Check for invalid energies or sectors
+		for value in args.values:
+			if not value in energies or value in sectors:
+				print u"\"{:s}\" är inte en giltig energityp eller sektor.".format(value)
+				exit()
 		length = (len(args.values))
 		output = ""
 		if length == 1:
 			source_energy = 0
 			for p in primaryenergies.values():
 				source_energy += p.value(args.values[0])[0]
-			(used, created) = energies[args.values[0]].value()
-			if source_energy != 0:
-				output += u"{:0.4f} TWh från alla primärenergier används för att framställa {:s}\n".format(source_energy, id_to_name[args.values[0]])
-			output += u"{:0.4f} TWh av energin från {:s} går till alla sektorer.\n".format(used, energies[args.values[0]].name)
-			output += u"Med detta så får man ut {:0.3f} TWh till alla sektorer.".format(created)
-		if length == 2:
+			if args.values[0] in energies:
+			# Checking value for an energy
+				(used, created) = energies[args.values[0]].value()
+
+				if source_energy != 0:
+					output += u"{:0.4f} TWh från alla primärenergier används för att framställa {:s}\n".format(source_energy, id_to_name[args.values[0]])
+				output += u"{:0.4f} TWh av energin från {:s} går till alla sektorer.\n".format(used, energies[args.values[0]].name)
+				output += u"Med detta så får man ut {:0.3f} TWh till alla sektorer.".format(created)
+			else:
+			# Checking value for a sector
+				used = source_energy
+				created = sectors[args.values[0]].value()
+				output += u"{:0.4f} TWh från alla primärenergier används för att framställa energi till {:s} sektorn\n".format(source_energy, id_to_name[args.values[0]])
+				output += u"Detta förser sektorn med {:0.4f} TWh energi.".format(created)
+		elif length == 2:
 			(used, created) = energies[args.values[0]].value(args.values[1])
 			output += u"{:0.4f} TWh av energin från {:s} går till {:s}.\n".format(used, energies[args.values[0]].name, id_to_name[args.values[1]])
 			output += u"Med detta så får man ut {:0.3f} TWh till {:s}.".format(created, id_to_name[args.values[1]])
