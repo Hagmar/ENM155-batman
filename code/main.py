53c3a0f5f6b79846dc82a759aacb4487ba837c4e
Magnus Hagmar <hagmar.magnus@gmail.com>
2015-01-06 14:06:19 +0100

    Forgot to remove le debug-print

--- code/main.py
+++ code/main.py
@@ -44,7 +44,6 @@ def main():
 			print u"{:16} id: {:15}".format(e.name, e.id)
 	elif args.values:
 		# Check for invalid energies or sectors
-		print sectors
 		for value in args.values:
 			if not (value in energies or value in sectors):
 				print u"\"{:s}\" är inte en giltig energityp eller sektor.".format(value)
